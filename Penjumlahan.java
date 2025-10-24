96774c1959aa80629270b2f356eaaccbf6ad9782
zaenabazikha <waodezaenabadzikhasagala@gmail.com>
2025-10-24 16:15:45 +0700

    first commit

---
+++ Penjumlahan.java
@@ -0,0 +1,9 @@
+public class Penjumlahan {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int hasil = a + b;
+
+        System.out.println("Hasil penjumlahan: " + hasil);
+    }
+}
